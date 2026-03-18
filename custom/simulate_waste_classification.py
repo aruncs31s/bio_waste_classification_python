154e2d467f20f128016841f6fb0afa95d6ea198f
Arun cs <aruncs31ss@gmail.com>
2026-03-18 20:51:35 +0530

    move uart implementations to seperate files

--- custom/simulate_waste_classification.py
+++ custom/simulate_waste_classification.py
@@ -131,35 +131,6 @@ class ServoPolicy:
                 self.actuator(duty)
                 return
 
-
-from abc import ABC, abstractmethod
-class UARTInterface(ABC):
-    @abstractmethod
-    def send(self, data: bytes) -> None:
-        pass
-
-class DummyUART(UARTInterface):
-    def send(self, data: bytes) -> None:
-        print(f"[UART] Sending data: {data.hex()}")
-
-
-from enum import Enum
-class ConvoyerBeltActions(Enum):
-    STOP = 1
-    START = 2
-   
-
-class PIUART(UARTInterface):
-    def __init__(self, port: str, baudrate: int = 9600):
-        try:
-            import serial
-            self.ser = serial.Serial(port, baudrate)
-        except Exception as e:
-            raise ImportError("Please install pyserial to use PIUART")
-    def send(self, data: ConvoyerBeltActions) -> None:
-        self.ser.write(data.value.to_bytes(1, byteorder='big'))
-
-
 @smart_inference_mode()
 def run(
     weights=str(ROOT) + "/" + "yolov5s.pt",  # model path or triton URL
---
+++ custom/uart_handlers.py
@@ -0,0 +1,27 @@
+
+from abc import ABC, abstractmethod
+class UARTInterface(ABC):
+    @abstractmethod
+    def send(self, data: bytes) -> None:
+        pass
+
+class DummyUART(UARTInterface):
+    def send(self, data: bytes) -> None:
+        print(f"[UART] Sending data: {data.hex()}")
+
+
+from enum import Enum
+class ConvoyerBeltActions(Enum):
+    STOP = 1
+    START = 2
+   
+
+class PIUART(UARTInterface):
+    def __init__(self, port: str, baudrate: int = 9600):
+        try:
+            import serial
+            self.ser = serial.Serial(port, baudrate)
+        except Exception as e:
+            raise ImportError("Please install pyserial to use PIUART")
+    def send(self, data: ConvoyerBeltActions) -> None:
+        self.ser.write(data.value.to_bytes(1, byteorder='big'))
