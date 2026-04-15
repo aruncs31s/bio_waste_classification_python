c3041f878e9cad169cffa30487b709da39eed4bd
UltralyticsAssistant <web@ultralytics.com>
2026-04-15 04:29:17 +0000

    Auto-format by https://ultralytics.com/actions

--- custom/file_handler.py
+++ custom/file_handler.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from utils.general import (
     cv2,
 )
+
+
 def save_results(dataset, vid_path, vid_writer, vid_cap, i, im0, save_path):
     if dataset.mode == "image":
         cv2.imwrite(save_path, im0)
@@ -19,4 +21,4 @@ def save_results(dataset, vid_path, vid_writer, vid_cap, i, im0, save_path):
                 fps, w, h = 30, im0.shape[1], im0.shape[0]
             save_path = str(Path(save_path).with_suffix(".mp4"))  # force *.mp4 suffix on results videos
             vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
-        vid_writer[i].write(im0) # update model (to fix SourceChangeWarning)
+        vid_writer[i].write(im0)  # update model (to fix SourceChangeWarning)
